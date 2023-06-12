9dff2c98b8d98ae84b617e684eec6f767703c947
RiaKristi <riakristibasri@gmail.com>
2023-06-12 18:01:13 +0700

    update template

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
 from flask import Flask, request, jsonify, render_template
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import books, users
+from . import models
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth(scheme='Bearer')
 
 # Fungsi untuk menghasilkan token berdasarkan username dan password
+
+
 def generate_token(username):
     return username + '_token'
 
 # Fungsi untuk verifikasi token
+
+
 def verify_token(token):
     return token.endswith('_token')
 
 # Fungsi untuk menghasilkan response saat autentikasi gagal
+
+
 @auth.error_handler
 def unauthorized():
     return jsonify({'error': 'Unauthorized access'}), 401
 
 # Fungsi untuk memverifikasi username dan password
+
+
 @auth.verify_password
 def verify_password(username, password):
     # Ganti kode ini dengan logika autentikasi yang sesuai, misalnya mengambil data pengguna dari database
@@ -28,32 +36,42 @@ def verify_password(username, password):
         return username
 
 # Fungsi untuk memverifikasi token
+
+
 @token_auth.verify_token
 def verify_token(token):
     return verify_token(token)
 
 # Halaman utama
+
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('books.html')
 
 # Halaman daftar buku
+
+
 @app.route('/books', methods=['GET'])
 @auth.login_required
 def get_books():
-    books = books.get_all_books()
+    books = models.get_all_books()
     return render_template('books.html', books=books)
 
 # Halaman detail buku
+
+
 @app.route('/books/<int:book_id>', methods=['GET'])
 @auth.login_required
 def get_book(book_id):
-    book = books.get_book_by_id(book_id)
+    book = models.get_book_by_id(book_id)
     if book:
         return render_template('book.html', book=book)
     return 'Book not found', 404
 
 # Endpoint untuk menambahkan buku baru
+
+
 @app.route('/books', methods=['POST'])
 @auth.login_required
 def add_book():
@@ -62,11 +80,13 @@ def add_book():
     author = data.get('author')
 
     if title and author:
-        books.insert_book(title, author)
+        models.insert_book(title, author)
         return 'Book added successfully', 201
     return 'Invalid request', 400
 
 # Endpoint untuk mengupdate buku
+
+
 @app.route('/books/<int:book_id>', methods=['PUT'])
 @auth.login_required
 def update_book(book_id):
@@ -75,30 +95,35 @@ def update_book(book_id):
     author = data.get('author')
 
     if title and author:
-        book = books.get_book_by_id(book_id)
+        book = models.get_book_by_id(book_id)
         if book:
-            books.update_book(book_id, title, author)
+            models.update_book(book_id, title, author)
             return 'Book updated successfully', 200
         return 'Book not found', 404
     return 'Invalid request', 400
 
 # Endpoint untuk menghapus buku
+
+
 @app.route('/books/<int:book_id>', methods=['DELETE'])
 @auth.login_required
 def remove_book(book_id):
-    book = books.get_book_by_id(book_id)
+    book = models.get_book_by_id(book_id)
     if book:
-        books.delete_book(book_id)
+        models.delete_book(book_id)
         return 'Book deleted successfully', 200
     return 'Book not found', 404
 
 # Endpoint untuk autentikasi dengan token
+
+
 @app.route('/auth/token', methods=['POST'])
 @auth.login_required
 def get_auth_token():
     token = generate_token(auth.current_user())
     return jsonify({'token': token})
 
+
 if __name__ == '__main__':
-    create_table()
+    models.create_table()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,40 +1,49 @@
-# Konfigurasi database SQLite
-DATABASE = 'books.db'
+import sqlite3
 
-# Fungsi untuk membuat koneksi ke database
-def get_db():
-    db = sqlite3.connect(DATABASE)
-    db.row_factory = sqlite3.Row
-    return db
+# Konfigurasi database SQLite
+conn = sqlite3.connect('books.db')
+cursor = conn.cursor()
+
 
 # Fungsi untuk membuat tabel books
 def create_table():
-    conn = get_db()
-    conn.execute('''
+    cursor.execute('''
         CREATE TABLE IF NOT EXISTS books (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT NOT NULL,
-            author TEXT NOT NULL
+            judul TEXT NOT NULL,
+            penulis TEXT NOT NULL,
+            tahun terbit TEXT NOT NULL,
+            penerbit TEXT NOT NULL,
         )
     ''')
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS users (id VARCHAR(255) PRIMARY KEY, username VARCHAR(50) UNIQUE, password VARCHAR(255))')
     conn.commit()
     conn.close()
 
 # Fungsi untuk menambahkan buku baru ke database
+
+
 def insert_book(title, author):
     conn = get_db()
-    conn.execute("INSERT INTO books (title, author) VALUES (?, ?)", (title, author))
+    conn.execute(
+        "INSERT INTO books (title, author) VALUES (?, ?)", (title, author))
     conn.commit()
     conn.close()
 
 # Fungsi untuk mengupdate buku berdasarkan ID
+
+
 def update_book(book_id, title, author):
     conn = get_db()
-    conn.execute("UPDATE books SET title = ?, author = ? WHERE id = ?", (title, author, book_id))
+    conn.execute("UPDATE books SET title = ?, author = ? WHERE id = ?",
+                 (title, author, book_id))
     conn.commit()
     conn.close()
 
 # Fungsi untuk menghapus buku berdasarkan ID
+
+
 def delete_book(book_id):
     conn = get_db()
     conn.execute("DELETE FROM books WHERE id = ?", (book_id,))
@@ -42,6 +51,8 @@ def delete_book(book_id):
     conn.close()
 
 # Fungsi untuk mendapatkan daftar semua buku
+
+
 def get_all_books():
     conn = get_db()
     books = conn.execute("SELECT * FROM books").fetchall()
@@ -49,8 +60,11 @@ def get_all_books():
     return books
 
 # Fungsi untuk mendapatkan detail buku berdasarkan ID
+
+
 def get_book_by_id(book_id):
     conn = get_db()
-    book = conn.execute("SELECT * FROM books WHERE id = ?", (book_id,)).fetchone()
+    book = conn.execute("SELECT * FROM books WHERE id = ?",
+                        (book_id,)).fetchone()
     conn.close()
     return book
